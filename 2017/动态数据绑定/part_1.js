a581e056b71e5cbdae8cd6a1ee0107721ccbe6ea
Linchengyi <m15659856340@163.com>
2017-03-18 15:59:29 +0800

    update Test Case

--- 2017/动态数据绑定/part_1.js
+++ 2017/动态数据绑定/part_1.js
@@ -1,30 +1,4 @@
 function Observer(obj) {
-    this.walk(obj); 
-}
-
-var p = Observer.prototype; //函数Observer的原型对象
-p.data = {} ;// 定义一个原型对象的属性data，这个属性被每个实例共享
-
-// 定义原型对象的方法walk，这个方法会在每个实例对象创建时被调用
-p.walk = function(obj) {
-  Object.keys(obj).forEach(function(key) {
-    if (typeof obj[key] === "object") {
-      return p.walk(obj[key]);
-    }
-  Object.defineProperty(p.data, key, {
-    get: function() {
-      console.log("You are visiting the attribute: " + key + " - " + obj[key]);
-      return obj[key];
-    },
-    set: function(newValue) {
-      console.log("You are updating the attribute: " + key + " - " + newValue);
-      obj[key] = newValue;
-    }
-  });
-});
-};
-
-function Observer(obj) {
   this.walk(obj);
 }
 
@@ -37,15 +11,22 @@ p.walk = function(obj) {
     if (typeof obj[key] === "object") {
       return p.walk(obj[key]);
     }
-  Object.defineProperty(p.data, key, {
-    get: function() {
-      console.log("You are visiting the attribute: " + key + " - " + obj[key]);
-      return obj[key];
-    },
-    set: function(newValue) {
-      console.log("You are updating the attribute: " + key + " - " + newValue);
-      obj[key] = newValue;
-    }
+    Object.defineProperty(p.data, key, {
+      get: function() {
+        console.log("You are visiting the attribute: " + key + " - " + obj[key]);
+        return obj[key];
+      },
+      set: function(newValue) {
+        console.log("You are updating the attribute: " + key + " - " + newValue);
+        obj[key] = newValue;
+      }
+    });
   });
-});
 };
+/* Test Case */
+var person1 = new Observer({
+             name:"lin", age:23, 
+             address:{add1:"China",add2:"FZ"} 
+             });
+person1.data.add1
+Console
